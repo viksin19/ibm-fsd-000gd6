efb0adba610bc3222c77a98b17f09c6dc7d9ad6b
b4ibmjava35@iiht.tech <b4ibmjava35@iiht.tech>
2019-09-25 09:44:43 +0530

    ...

--- Project-Management-System/Project/Project-service/src/main/java/com/example/demo/controller/ProjectController.java
+++ Project-Management-System/Project/Project-service/src/main/java/com/example/demo/controller/ProjectController.java
@@ -11,7 +11,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -40,7 +42,7 @@ public class ProjectController {
 	}
 
 	@GetMapping("/findbyid")
-	public ResponseEntity<Project> findById(@RequestParam("id") int id) {
+	public ResponseEntity<Project> findById(@RequestParam("id") long id) {
 
 		return ResponseEntity.ok(service.findById(id));
 	}
@@ -63,11 +65,20 @@ public class ProjectController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(proRes);
 	}
 
-	@PostMapping("/deleteproject")
-	public ResponseEntity<?> deleteProject(@RequestParam("id") int id) {
+	@GetMapping("/deleteproject")
+	public ResponseEntity<?> deleteproject(@RequestParam("id") long id) {
 		service.deleteProject(id);
 		return ResponseEntity.ok("deleted");
 	}
+	@PostMapping("/project/{id}")
+    public ResponseEntity<CreateProjectResponseModel> updateProject(@PathVariable("id") int id, @RequestBody CreateProjectRequestModel update) throws ParseException{
+    	ModelMapper mapper = new ModelMapper();
+		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		ProjectDto dto = mapper.map(update, ProjectDto.class);
+		ProjectDto dto1 = service.updateProject(dto, id);
+		CreateProjectResponseModel user = mapper.map(dto1, CreateProjectResponseModel.class);
+		return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
 	
 	@PostMapping("/projects/tasks")
 	public ResponseEntity<?> createTeam(@RequestBody CreateTasksRequestModel taskDetail) {
