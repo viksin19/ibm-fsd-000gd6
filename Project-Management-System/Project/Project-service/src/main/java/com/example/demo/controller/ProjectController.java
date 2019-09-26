20540295089041a0137461b521d776aa37ee061d
TheRohitChoubey <rohitc.choubey.com>
2019-09-26 17:35:52 +0530

    New backend

---
+++ Project-Management-System/Project/Project-service/src/main/java/com/example/demo/controller/ProjectController.java
@@ -0,0 +1,91 @@
+package com.example.demo.controller;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Project;
+import com.example.demo.feignClient.ProjectFeignClient;
+import com.example.demo.model.CreateProjectRequestModel;
+import com.example.demo.model.CreateProjectResponseModel;
+import com.example.demo.model.CreateTasksRequestModel;
+import com.example.demo.model.CreateTasksResponseModel;
+import com.example.demo.service.ProjectService;
+import com.example.demo.shared.ProjectDto;
+import com.example.demo.shared.TasksDto;
+
+@RestController
+public class ProjectController {
+	@Autowired
+	private ProjectService service;
+	
+
+	@RequestMapping("/projects/getallproject")
+	public ResponseEntity<List<Project>> getAllproject() {
+
+		return ResponseEntity.ok(service.findAll());
+	}
+
+	@GetMapping("/findbyid")
+	public ResponseEntity<Project> findById(@RequestParam("id") Long id) {
+
+		return ResponseEntity.ok(service.findById(id));
+	}
+	
+	@GetMapping("/findbyemail/{email}")
+	public ResponseEntity<?> findByEmail(@PathVariable("email") String email) {
+
+		return ResponseEntity.ok(service.getByEmail(email));
+	}
+
+	@GetMapping("/findbyloction")
+	public ResponseEntity<List<Project>> projectbyLoction(@RequestParam("plocation") String plocation) {
+
+		return ResponseEntity.ok(service.findBylocation(plocation));
+	}
+
+	@PostMapping("/projects")
+	public ResponseEntity<CreateProjectResponseModel> create(@RequestBody ArrayList<CreateProjectRequestModel> pData )
+			throws ParseException {
+		// TODO Auto-generated method stub
+		ModelMapper model = new ModelMapper();
+		model.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		System.out.println(pData.get(0).toString());
+		CreateProjectRequestModel projectreq = pData.get(0);
+		ProjectDto pDto = model.map(projectreq, ProjectDto.class);
+		ProjectDto tempDto = service.saveProject(pDto);
+		CreateProjectResponseModel proRes = model.map(tempDto, CreateProjectResponseModel.class);
+		return ResponseEntity.status(HttpStatus.CREATED).body(proRes);
+	}
+
+	@PostMapping("/deleteproject")
+	public ResponseEntity<?> deleteProject(@RequestParam("id") Long id) {
+		service.deleteProject(id);
+		return ResponseEntity.ok("deleted");
+	}
+	
+	@PostMapping("/projects/tasks")
+	public ResponseEntity<?> createTeam(@RequestBody CreateTasksRequestModel taskDetail) {
+		//CreateTasksRequestModel taskDetail = task.get(0);
+		TasksDto tempDto = service.createTask(taskDetail);
+		ModelMapper model = new ModelMapper();
+		model.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		CreateTasksResponseModel tmodel = model.map(tempDto, CreateTasksResponseModel.class);
+		return ResponseEntity.status(HttpStatus.CREATED).body(tmodel);
+	}
+}
---
+++ Project-Management-System/Project/Project-service/src/main/java/com/example/demo/service/ProjectServiceImpl.java
@@ -0,0 +1,88 @@
+package com.example.demo.service;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.UUID;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Project;
+import com.example.demo.feignClient.ProjectFeignClient;
+import com.example.demo.model.CreateTasksRequestModel;
+import com.example.demo.repository.ProjectRepository;
+import com.example.demo.shared.ProjectDto;
+import com.example.demo.shared.TasksDto;
+
+@Service
+public class ProjectServiceImpl implements ProjectService {
+	
+	private ProjectRepository projectRepository;
+	@Autowired
+	private ProjectFeignClient projectFeignClient;
+	
+	
+	@Autowired
+	public ProjectServiceImpl(ProjectRepository projectRepository, ProjectFeignClient projectFeignClient) {
+		super();
+		this.projectRepository = projectRepository;
+		this.projectFeignClient = projectFeignClient;
+	}
+
+	@Override
+	public List<Project> findAll() {
+
+		return (List<Project>) projectRepository.findAll();
+	}
+
+	@Override
+	public List<Project> findBylocation(String plocation) {
+		// TODO Auto-generated method stub
+		return projectRepository.findByplocation(plocation);
+	}
+
+	@Override
+	public Project findById(Long id) {
+		// TODO Auto-generated method stub
+		return projectRepository.findById(id).get();
+	}
+
+	@Override
+	public ProjectDto saveProject(ProjectDto projectDto) throws ParseException {
+		// TODO Auto-generated method stub
+		//projectDto.setStart_date(date.parse(projectDto.getsDate()));
+		//projectDto.setEnd_date(date.parse(projectDto.geteDate()));
+		
+		ModelMapper model = new ModelMapper();
+		model.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		Project project = model.map(projectDto, Project.class);
+		System.out.println(project);
+		projectRepository.save(project);
+		ProjectDto proDto = model.map(project, ProjectDto.class);
+		return proDto;
+	}
+
+	@Override
+	public void deleteProject(Long id) {
+		// TODO Auto-generated method stub
+		projectRepository.deleteById(id);
+	}
+
+	@Override
+	public TasksDto createTask(CreateTasksRequestModel taskDetail) {
+		System.out.println(taskDetail.toString());
+		ResponseEntity<TasksDto> tDto =  (ResponseEntity<TasksDto>) projectFeignClient.createTask(taskDetail);
+		TasksDto taskDto =  tDto.getBody();
+		return taskDto;
+	}
+
+	@Override
+	public List<Project> getByEmail(String email) {
+		return projectRepository.findByPmanagerEmail(email);
+	}
+
+}
