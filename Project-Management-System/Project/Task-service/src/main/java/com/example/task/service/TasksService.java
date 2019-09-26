db244e88b9c250c5ac1bbc78fd8cdb97107de5db
TheRohitChoubey <rohitc.choubey.com>
2019-09-26 11:28:59 +0530

    New changes

--- Project-Management-System/Project/Task-service/src/main/java/com/example/task/service/TasksService.java
+++
@@ -1,9 +0,0 @@
-package com.example.task.service;
-
-import com.example.task.shared.TasksDto;
-
-public interface TasksService {
-	
-	public TasksDto createNewTask(TasksDto taskDetail);
-	public TasksDto getTask(String taskName);
-}
