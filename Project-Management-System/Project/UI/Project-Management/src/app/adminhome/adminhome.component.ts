d1795820e7a75a7535a0c41f7d907196c5275334
b4ibmjava03@iiht.tech <b4ibmjava03@iiht.tech>
2019-10-07 10:50:19 +0530

    angular

--- Project-Management-System/Project/UI/Project-Management/src/app/adminhome/adminhome.component.ts
+++ Project-Management-System/Project/UI/Project-Management/src/app/adminhome/adminhome.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../Interfaces/User';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from '../user.service';
+import { Project } from '../Interfaces/Project';
 
 @Component({
   selector: 'app-adminhome',
@@ -17,24 +18,26 @@ export class AdminhomeComponent implements OnInit {
   engagedUser: number
   arr:any[]
   admin:string
-
+  memberCount:any[]
+  projects:Project[]
+  
 
   public barOptions = {
     scaleShowVerticalLines: false,
     responsive: true
   };
-  public barLables = ['AT&T', 'Northern-Trust', 'Red-Hat'];
+  public barLables = [];
   public barChartType = 'bar';
   public barLegends = true;
   public barData = [
-    { data: [60, 98, 40], label: 'Tasks' },
-    { data: [45, 50, 25], label: 'Task-completed' }
+    { data: this.memberCount, label: 'Members' }
+   
   ];
 
 
   public doughnutLables = ['Projects-Completed', 'Live-Projects'];
   public doughnutChartType = 'doughnut';
-  public doughnutData = [40, 80];
+  public doughnutData = [4,6];
 
   public pieLabels = ['Manager','Free-Developer','Enganged-Developer'];
   public pieData = [ , , , ];
@@ -74,6 +77,19 @@ export class AdminhomeComponent implements OnInit {
           .then(data => {
             this.graphUser = data;
             this.graphsUser();
+
+
+            fetch(`http://b4ibm21.iiht.tech:8010/projects/getallproject`,{
+              method:"GET",
+              headers:{
+                "Content-Type":"application/json"
+              }
+            }).then(pro=>pro.json())
+              .then(pro=>{
+                  this.projects=pro;
+                  this.proGraph();
+
+              })
           })
 
       })
@@ -99,6 +115,35 @@ export class AdminhomeComponent implements OnInit {
   }
   localCheck() {
   
+  }
+  proGraph(){
+     
+    for ( let i=0; i<this.projects.length;i++){
+      if(i==0){
+        this.barLables=[this.projects[i].pname]
+      }else{
+      this.barLables.push(this.projects[i].pname);
+      }
+      
+      fetch(`http://b4ibm21.iiht.tech:8001/findByProjectId/${this.projects[i].projectId}`,{
+        method:"GET",
+        headers:{
+          "Content-Type":"application/json"
+        }
+      }).then(res=>res.json())
+        .then(res=>{
+          console.log(res);
+          let result:User[]
+          result=res;
+          if(i==0){
+            this.memberCount=[result.length];
+          }else{
+          this.memberCount.push(result.length);
+          }
+        })
+    }
+    console.log(this.barLables);
+    console.log(this.barData);
   }
 
   
